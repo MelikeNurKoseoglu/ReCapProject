354de6d1821a5283a8771d0da4601acb10e4a160
Melike Nur Köseoğlu <koseoglumelikenur@gmail.com>
2022-03-14 19:48:09 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EFCarDal.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EFCarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            using (CarDBContext context= new CarDBContext())
+            {
+                var adededEntity = context.Entry(entity);
+                adededEntity.State = EntityState.Added;
+                context.SaveChanges();
+
+            }
+        }
+
+        public void Delete(Car entity)
+        {
+            using (CarDBContext context=new CarDBContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            using (CarDBContext context = new CarDBContext())
+            {
+                return filter == null
+                    ? context.Set<Car>().ToList()
+                    : context.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+        public Car GetT(Expression<Func<Car, bool>> filter = null)
+        {
+            using (CarDBContext context = new CarDBContext())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+            }
+        }
+
+        public void Update(Car entity)
+        {
+            using (CarDBContext context=new CarDBContext())
+            {
+                var updated=context.Entry(entity);
+                updated.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal:ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=23,ModelYear=2000,DailyPrice=30000,Decription="Araba 1"},
+                new Car{CarId=2,BrandId=1,ColorId=45,ModelYear=2020,DailyPrice=200000,Decription="Araba 2"},
+                new Car{CarId=3,BrandId=2,ColorId=9,ModelYear=2015,DailyPrice=98000,Decription="Araba 3"},
+                new Car{CarId=4,BrandId=2,ColorId=3,ModelYear=2005,DailyPrice=78000,Decription="Araba 4"},
+                new Car{CarId=5,BrandId=2,ColorId=43,ModelYear=1980,DailyPrice=3000000,Decription="Araba 5"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAllByCategory(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public Car GetT(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate=_cars.SingleOrDefault(c1=> c1.CarId == car.CarId);
+            carToUpdate.BrandId= car.BrandId;
+            carToUpdate.ColorId= car.ColorId;
+            carToUpdate.ModelYear= car.ModelYear;
+            carToUpdate.DailyPrice=car.DailyPrice;
+            carToUpdate.Decription=car.Decription;
+
+        }
+    }
+}
