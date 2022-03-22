a2d2dedbb03f39b3280caf181cded156414aac8c
Melike Nur Köseoğlu <koseoglumelikenur@gmail.com>
2022-03-22 18:52:59 +0300

    DTOs completed

---
+++ Entities/DTOs/CarDetailDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class CarDetailDto
+    {
+        public int CarId { get; set; }
+        public string BrandName { get; set; }
+        public string ColorNmae { get; set; }
+        public int DailyPrice { get; set; }
+    }
+}
